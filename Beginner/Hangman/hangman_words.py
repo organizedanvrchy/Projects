275c0c873711c16c9d58cc20861f90eec0d82ede
Anvrchy <154699780+organizedanvrchy@users.noreply.github.com>
2024-11-18 00:05:15 -0500

    Create hangman_words.py

---
+++ Beginner/Hangman/hangman_words.py
@@ -0,0 +1,16 @@
+word_list = [
+    "aardvark", "baboon", "camel", "apple", "zoo", "business", "hello", "elephant", 
+    "giraffe", "hippopotamus", "kangaroo", "lion", "monkey", "ostrich", "panda", 
+    "quail", "rabbit", "snake", "tiger", "umbrella", "vulture", "whale", "xenon", 
+    "yellow", "zebra", "dinosaur", "computer", "keyboard", "programming", "internet", 
+    "python", "java", "algorithm", "data", "structure", "machine", "learning", 
+    "science", "robot", "artificial", "intelligence", "gadget", "smartphone", "tablet", 
+    "charger", "laptop", "camera", "headphones", "wireless", "network", "battery", 
+    "technology", "software", "hardware", "monitor", "printer", "scanner", "mammal", 
+    "reptile", "amphibian", "bird", "fish", "insect", "flower", "tree", "mountain", 
+    "ocean", "forest", "desert", "island", "continent", "planet", "star", "galaxy", 
+    "universe", "cosmos", "space", "astronaut", "rocket", "satellite", "telescope", 
+    "moon", "sun", "earth", "mars", "venus", "jupiter", "saturn", "neptune", "pluto", 
+    "mercury", "robotics", "cyber", "quantum", "physics", "chemistry", "biology", 
+    "astronomy", "geology", "meteorology", "geography", "history", "politics", "economics"
+    ]
