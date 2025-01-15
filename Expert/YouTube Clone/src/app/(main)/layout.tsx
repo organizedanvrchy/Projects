c839f8eaf23621480bd6ccde44bba427e8587612
Anvrchy <154699780+organizedanvrchy@users.noreply.github.com>
2025-01-15 14:48:36 -0500

    Add files via upload

--- Expert/YouTube Clone/src/app/(main)/layout.tsx
+++ Expert/YouTube Clone/src/app/(main)/layout.tsx
@@ -7,11 +7,11 @@ export default function MainLayout({
     children: React.ReactNode;
 }) {
     return (
-    <>
-        <main className="md:pl-60 pt-36">
-            <div>{children}</div>
-        </main>
-        <Sidebar className='translate-x-0' />
-    </>
+        <>
+            <main className="md:pl-60 pt-36">
+                <div>{children}</div>
+            </main>
+            <Sidebar className="translate-x-0" />
+        </>
     );
 }
