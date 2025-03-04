f266f5daac04891033dac2bea4faaac54ae40d13
Vimal <154699780+organizedanvrchy@users.noreply.github.com>
2025-03-04 01:59:51 -0500

    Delete Intermediate directory

--- Intermediate/Coffee Machine/resources.py
+++
@@ -1,8 +0,0 @@
-# resources.py
-
-# Define the initial amount of resources available in the machine
-resources = {
-    "water": 500,
-    "milk": 400,
-    "coffee": 100,
-}
--- Intermediate/Quiz Game/question_model.py
+++
@@ -1,5 +0,0 @@
-class Question:
-    def __init__(self, text, answer, difficulty):
-        self.text = text
-        self.answer = answer
-        self.difficulty = difficulty
